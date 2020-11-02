2525ff6c7a1a859b15fc011cb29cf56136593418
Sumit Jaiswal <54527500+ransomsumit@users.noreply.github.com>
2020-11-02 23:49:43 +0530

    username bug fixed

--- main.py
+++ main.py
@@ -51,8 +51,11 @@ def on_chat_message(msg):
                         inl.append([InlineKeyboardButton(text=str(link)[:18] + "....."+ str(link)[-18:], url="https://gogoanime.so/"+str(lob)[10:]+"-episode-1")])
                     else:
                         inl.append([InlineKeyboardButton(text=str(link), parse_mode='Markdown', callback_data=str(lob)[10:]+"about")])
-
-                bot.sendMessage('1152801694',msg['text'] +" "+ msg['from']['first_name'] +" " + msg['from']['username'])
+                if('username' in msg['from']):
+                    bot.sendMessage('1152801694', msg['text'] +" "+ msg['from']['first_name']+" @" + msg['from']['username'])
+                else:
+                    bot.sendMessage('1152801694', msg['text'] +" "+ msg['from']['first_name'])
+                    
                 if(group_id==chat_id):
                     bot.sendMessage(chat_id,"RESULTS",reply_markup = InlineKeyboardMarkup(inline_keyboard=inl))
                 else:
