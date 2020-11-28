b7d74b6c69e2d506b7dea98a040ed7fcdf7182e5
Sumit Jaiswal <54527500+ransomsumit@users.noreply.github.com>
2020-11-28 11:13:57 +0530

    emoji guess bug fix

--- list_manager.py
+++ list_manager.py
@@ -3,18 +3,21 @@ lst={}
 def adder(chat_id, name):
     lst[chat_id] = name
 def list_search(x, chat_id):
+    m = re.sub(r'[^\w]', ' ', lst[chat_id].lower())
     if(x == ""):
         return False
     else:
         x = re.sub(r'[^\w]', ' ', x)
         x = x.strip().split()
         flag = 0
-        for i in x:
-            m = re.sub(r'[^\w]', ' ', lst[chat_id].lower())
-            if i in m.split():
-                flag = 0
-            else:
-                flag = 1
+        if(len(x)==0):
+            return False
+        else:
+            for i in x:
+                if i in m.split():
+                    flag = 0
+                else:
+                    flag = 1
         if flag == 1:
             return False
         else:
